e86f3c2f95895954f7b6f19824a28208e857f631
r2-ortiz <r2-ortizrodrig@live.uwe.ac.uk>
2022-10-12 16:49:23 +0100

    Slider fit in the screen of my new SimpleEQAudioProcessor Editor
    Before being connected to our Parameters

--- SimpleEQ/Source/PluginEditor.h
+++ SimpleEQ/Source/PluginEditor.h
@@ -11,6 +11,15 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider   //To dont type this base class initialisation for every slider, this class do this in the constructor. So I can use this for every slider in the GUI
+{
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +37,11 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    CustomRotarySlider peakFreakSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider,
+                       lowCutSlope, highCutSlope;
+
+    std::vector<juce::Component*> getComps(); //To have all the slider in a vector because I want to have done the same all the time to them (like makethemVisible)
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
--- SimpleEQ/Source/PluginProcessor.cpp
+++ SimpleEQ/Source/PluginProcessor.cpp
@@ -335,8 +335,8 @@ bool SimpleEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
-  // return new SimpleEQAudioProcessorEditor (*this);
-    return new GenericAudioProcessorEditor(*this);
+   return new SimpleEQAudioProcessorEditor (*this);
+  //  return new GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -345,12 +345,21 @@ void SimpleEQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);  //To save the parameters of the GUI in a memory
 }
 
 void SimpleEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes); //Read that memory with the data of the parameters saved
+    if (tree.isValid()) //Check if the tree that was pulled from memory is valid befre we copy it to out plugins state
+    {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 
@@ -386,7 +395,7 @@ void SimpleEQAudioProcessor::updateCoefficients(Coefficients& old, const Coeffic
 {
     *old = *replacements;
 }
-
+//Commented on Pre Play and Process Block:
 void SimpleEQAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings)
 {
     auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq,
